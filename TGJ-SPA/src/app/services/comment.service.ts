a828b98ad417591a3e3cfb257d1138bba1037509
jaaferh <jaaferluay@gmail.com>
2021-03-15 22:07:00 +0000

    Added Comment service

---
+++ TGJ-SPA/src/app/services/comment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Comment } from '../models/comment.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  createComment(comment: Comment, postId: string): Observable<Comment> {
+    return this.http.post<Comment>(this.baseUrl + 'comment/create', {...comment, postId});
+  }
+
+  deleteComment(commentId: string): Observable<void> {
+    return this.http.delete<void>(this.baseUrl + 'comment/' + commentId + '/delete');
+  }
+
+}
