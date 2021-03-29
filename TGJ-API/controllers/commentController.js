e5d17c9905eb15729617312221d5fbe7139089d7
jaaferh <jaaferluay@gmail.com>
2021-03-29 22:49:11 +0100

    Added Comment API update method

--- TGJ-API/controllers/commentController.js
+++ TGJ-API/controllers/commentController.js
@@ -43,6 +43,26 @@ exports.comment_delete = (req, res, next) => {
 };
 
 // UPDATE POST.
-exports.comment_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: comment update POST');
+exports.comment_update_post = (req, res, next) => {
+  // Create a Topic object with data and old id
+  const comment = new Comment(
+    {
+      post: req.body.post,
+      parent_comment: req.body.parent_comment,
+      author_nickname: req.body.author_nickname,
+      text: req.body.text,
+      date_posted: req.body.date_posted,
+      last_edited: req.body.last_edited,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      _id: req.params.id,
+    },
+  );
+
+  // Update the record
+  Comment.findByIdAndUpdate(req.params.id, comment, {}, (err) => {
+    if (err) { return next(err); }
+    // Successful - set OK status
+    return res.status(200).end();
+  });
 };
