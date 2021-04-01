e5ebe2631b80de9fe9ebdc2c965208cbfa68290c
jaaferh <jaaferluay@gmail.com>
2021-04-01 22:16:45 +0100

    Add angular-fontawesome

--- TGJ-SPA/src/app/app.module.ts
+++ TGJ-SPA/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatAutocompleteModule,
     MatSelectModule,
     MatFormFieldModule,
+    FontAwesomeModule,
   ],
   providers: [
     AlertifyService,
