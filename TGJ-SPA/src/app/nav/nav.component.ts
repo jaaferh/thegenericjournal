399c0ed716cf2d02be0873de5326f49ba4ee28ac
Jaafer <jaaferluay@gmail.com>
2021-05-15 13:22:30 +0100

    Sticky Navbar + Navbar link hover animations

--- TGJ-SPA/src/app/nav/nav.component.ts
+++ TGJ-SPA/src/app/nav/nav.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-
+export class NavComponent implements AfterViewInit {
+  @ViewChild('navbar') navbar!: ElementRef;
+  @HostListener('window:scroll', ['$event'])
+    scrollHandler(event: any) {
+      this.checkSticky();
+    }
+  stick = false;
+  navbarTopOffset = 0;
   constructor() { }
 
-  ngOnInit() {
+  // ngOnInit(): void {
+  //   console.log(this.navbar.nativeElement.getBoundingClientRect());
+  // }
+
+  ngAfterViewInit(): void {
+    this.navbarTopOffset = this.navbar.nativeElement.getBoundingClientRect().top;
   }
+
+  checkSticky(): void {
+    // const navbarTopOffset = this.navbar.nativeElement.getBoundingClientRect().top;
+    this.stick = window.pageYOffset > this.navbarTopOffset ? true : false;
+    console.log('window ' + window.pageYOffset);
+    console.log('navbar ' + this.navbarTopOffset);
+    // console.log(this.stick);
+  }
+
+  
 
 }
