cb52062c22e0c6167d9dccfd8386418759dafb47
Jaafer <jaaferluay@gmail.com>
2021-06-14 22:22:49 +0100

    Initialising firebase in SPA and deploy

--- TGJ-SPA/src/environments/environment.prod.ts
+++ TGJ-SPA/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'https://vast-forest-20532.herokuapp.com/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBPtuzE0S2LNgAMURyPsFnBcOeu6WKEzAo",
+    authDomain: "the-generic-journal.firebaseapp.com",
+    projectId: "the-generic-journal",
+    storageBucket: "the-generic-journal.appspot.com",
+    messagingSenderId: "40458506568",
+    appId: "1:40458506568:web:8a1e7abac013b3e670637d"
+  }
 };
