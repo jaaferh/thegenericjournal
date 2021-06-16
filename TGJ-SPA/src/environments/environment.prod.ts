26e1cd75cdc3922259cfd4d5c401dc8df4cc5994
Jaafer <jaaferluay@gmail.com>
2021-06-16 21:21:03 +0100

    Fixed prod apiurl

--- TGJ-SPA/src/environments/environment.prod.ts
+++ TGJ-SPA/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vast-forest-20532.herokuapp.com/',
+  apiUrl: 'https://vast-forest-20532.herokuapp.com/journal/',
   firebaseConfig: {
     apiKey: "AIzaSyBPtuzE0S2LNgAMURyPsFnBcOeu6WKEzAo",
     authDomain: "the-generic-journal.firebaseapp.com",
