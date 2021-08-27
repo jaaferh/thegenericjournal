30ea7735289d89851606ae1ac8482df85b761b0e
Jaafer <jaaferluay@gmail.com>
2021-08-27 19:46:40 +0100

    Login and Register unit tests

--- TGJ-SPA/src/app/user/login/login.component.spec.ts
+++ TGJ-SPA/src/app/user/login/login.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ToasterService } from 'angular2-toaster';
+import { of } from 'rxjs';
+import { User, UserToken } from 'src/app/models/user.entity';
 import { UserService } from 'src/app/services/user.service';
 
 import { LoginComponent } from './login.component';
@@ -8,21 +11,21 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let userService: UserService;
   
   const toasterSpy = {
     pop: jasmine.createSpy('pop')
   };
-  const userSpy = {
-    loginUser: jasmine.createSpy('loginUser')
-  };
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ FormsModule ],
+      imports: [ FormsModule, HttpClientTestingModule ],
       providers: [
         { provide: ToasterService, useValue: toasterSpy },
-        { provide: UserService, useValue: userSpy }
+        UserService,
+        // { provide: WINDOW_TOKEN, useValue: { location: { reload: () => {} } } }
       ]
     })
     .compileComponents();
@@ -32,11 +35,32 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    userService = TestBed.inject(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  /* Cant test onSubmit() due to window.location.href. Need to use 
+     OpaqueToken to inject WindowToken into the component, then spy on
+     the WindowToken here to prevent it from actually reloading the page. */
+  it('should login on submit', () => {
+    component.login = {
+      email: 'email@email.com',
+      password: 'password123'
+    };
+
+    const userToken: UserToken = {
+      user: {} as User,
+      token: 'testToken'
+    };
+
+    spyOn(userService, 'loginUser').and.returnValue(of(userToken));
+
+    // component.onSubmit(); 
+    // expect(toasterSpy.pop).toHaveBeenCalled();
+    // expect(userSpy.loginUser(component.login)).toHaveBeenCalled();
+  });
 
 });
--- TGJ-SPA/src/app/user/register/register.component.spec.ts
+++ TGJ-SPA/src/app/user/register/register.component.spec.ts
@@ -1,18 +1,33 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ToasterService } from 'angular2-toaster';
+import { EMPTY, from, Observable, of, throwError } from 'rxjs';
+import { Register } from 'src/app/models/user.entity';
+import { UserService } from 'src/app/services/user.service';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let userService: UserService;
+  let router: Router;
+
+  const toasterSpy = {
+    pop: jasmine.createSpy('pop')
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
-      imports: [ HttpClientTestingModule, RouterTestingModule, FormsModule ] // need to flesh out RTM
+      imports: [ HttpClientTestingModule, RouterTestingModule.withRoutes([]), FormsModule ], // need to flesh out RTM
+      providers: [ 
+        { provide: ToasterService, useValue: toasterSpy },
+        UserService,
+      ]
     })
     .compileComponents();
   });
@@ -21,9 +36,60 @@ describe('RegisterComponent', () => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    userService = TestBed.inject(UserService);
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should register a user on submit then navigate to the login page', () => {
+    const register: Register = {
+      first_name: 'affendim',
+      family_name: 'pasha',
+      date_of_birth: new Date('2021-08-13'),
+      password: 'testpass123',
+      confirm_pass: 'testpass123',
+      email: 'testemail@email.com'
+    };
+    component.register = register;
+
+    // returnValue must be of<any>({}) for Observable<void> for next (subscribe innards) to be called;
+    const spy = spyOn(userService, 'registerUser').and.returnValue(of<any>({}));
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.onSubmit();
+
+    expect(spy).toHaveBeenCalled();
+
+    expect(toasterSpy.pop).toHaveBeenCalledWith('success', 'Successfully Registered. Please Log In');
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+
+  });
+
+  it('should toast an error when register fails', () => {
+    const register: Register = {
+      first_name: 'affendim',
+      family_name: 'pasha',
+      date_of_birth: new Date('2021-08-13'),
+      password: 'testpass123',
+      confirm_pass: 'testpass123',
+      email: 'testemail@email.com'
+    };
+    component.register = register;
+
+    const spy = spyOn(userService, 'registerUser').and.returnValue(throwError({status: 404}));
+
+    component.onSubmit();
+
+    expect(toasterSpy.pop).toHaveBeenCalledWith('error', Object({status: 404}));
+
+  });
+
+  it('should set the date of birth on date change', () => {
+    expect(component.register.date_of_birth).toBeFalsy();
+    component.dateChange('2021-08-13');
+    expect(component.register.date_of_birth).toBeTruthy();
   });
 });
